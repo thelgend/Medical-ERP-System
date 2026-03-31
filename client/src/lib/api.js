d816021de06a449e75c3fae5f4e7d3650b8086aa
MrMasa <m00773109@gmail.com>
2026-03-31 06:50:51 +0200

    Update api.js

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: '/api',
 });
 
 // Add a request interceptor to add the auth token to every request
